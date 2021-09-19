bef53f3893ef222fb0c1cd37ccd7c35b7d5595fe
strayge <strayge@gmail.com>
2021-09-20 02:30:55 +0300

    initial

---
+++ README.md
@@ -0,0 +1,99 @@
+## Requirements
+
+#### 1. python packages
+```sh
+pip install -r -requirements.txt
+```
+#### 2. tesseract
+
+Windows installer available [here](https://github.com/UB-Mannheim/tesseract/actions/runs/1119350562)
+(required github login)
+
+#### 3. chrome driver
+
+download from [here](https://chromedriver.chromium.org/downloads)
+
+### Options
+```
+usage: main.py [-h] [--url URL] [--input urls.txt] [--output out.json] [--print]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --url URL, -u URL     pass single url
+  --input urls.txt, -i urls.txt
+                        get urls from file (one per line)
+  --output out.json, -o out.json
+                        save results in json format
+  --print, -p           print to stdout each page result
+```
+
+### Example
+
+#### single url in terminal
+```sh
+python main.py -u 'http://www.primorsk.vybory.izbirkom.ru/region/izbirkom?action=show&root=12000001&tvd=4014001103304&vrn=100100067795849&prver=0&pronetvd=null&region=1&sub_region=1&type=242&report_mode=null'
+```
+
+#### batch processing
+```sh
+python main.py -i in.txt -o results.json
+```
+
+#### JSON output example
+
+```json
+{
+  "http://www.primorsk.vybory.izbirkom.ru/region/izbirkom?action\u003dshow\u0026root\u003d552000002\u0026tvd\u003d4554026217797\u0026vrn\u003d100100067795849\u0026prver\u003d0\u0026pronetvd\u003dnull\u0026region\u003d55\u0026sub_region\u003d55\u0026type\u003d242\u0026report_mode\u003dnull\n": [
+    [
+      "Дата голосования:  18.09.2016"
+    ],
+    [
+      "Наименование избирательной комиссии",
+      "УИК №370"
+    ],
+    [
+      "Число избирателей, внесенных в список избирателей на момент окончания голосования",
+      "2342"
+    ],
+    ...
+    [
+      "Политическая партия \"ПАТРИОТЫ РОССИИ\"",
+      "2",
+      "0.32%"
+    ],
+    [
+      "Политическая партия СПРАВЕДЛИВАЯ РОССИЯ",
+      "49",
+      "7.13%"
+    ]
+  ],
+  "http://www.primorsk.vybory.izbirkom.ru/region/izbirkom?action\u003dshow\u0026root\u003d12000001\u0026tvd\u003d4014001103304\u0026vrn\u003d100100067795849\u0026prver\u003d0\u0026pronetvd\u003dnull\u0026region\u003d1\u0026sub_region\u003d1\u0026type\u003d242\u0026report_mode\u003dnull\n": [
+    [
+      "Дата голосования:  18.09.2016"
+    ],
+    [
+      "Наименование избирательной комиссии",
+      "УИК №1"
+    ],
+    [
+      "Число избирателей, внесенных в список избирателей на момент окончания голосования",
+      "2584"
+    ],
+    [
+      "Число избирательных бюллетеней, полученных участковой избирательной комиссией",
+      "2328"
+    ],
+    ...
+    [
+      "Политическая партия \"ПАТРИОТЫ РОССИИ\"",
+      "0",
+      "0.00%"
+    ],
+    [
+      "Политическая партия СПРАВЕДЛИВАЯ РОССИЯ",
+      "56",
+      "2.65%"
+    ]
+  ]
+}
+```
---
+++ main.py
@@ -0,0 +1,98 @@
+import json
+from base64 import b64decode
+from io import BytesIO
+from time import sleep
+from typing import List
+
+import pytesseract
+from PIL import Image
+from selenium.webdriver import Chrome, ChromeOptions
+
+
+def init_driver() -> Chrome:
+    options = ChromeOptions()
+    options.headless = True
+    driver = Chrome(options=options)
+    driver.set_window_size(3000, 4000)
+    return driver
+
+
+def get_page_data(driver: Chrome, url: str) -> List[List[str]]:
+    driver.delete_all_cookies()
+    driver.get(url)
+    sleep(1)
+
+    # increase text size
+    script = 'document.styleSheets[0].insertRule("body {font-size: 2em !important;}", 0 )'
+    driver.execute_script(script)
+
+    results = []
+    head_rows = driver.find_elements_by_css_selector('.table-borderless tr')
+    for row in head_rows:
+        cols = row.find_elements_by_tag_name('td')
+        line = []
+        for col in cols:
+            text = col.text.strip()
+            if text:
+                line.append(text)
+        if line:
+            results.append(line)
+
+    rows = driver.find_elements_by_css_selector('.table-responsive tr')
+
+    for i, row in enumerate(rows):
+        cols = row.find_elements_by_tag_name('td')
+        col2 = cols[1].text.strip()
+        if '. ' in col2[:10]:
+            # cut numeration
+            col2 = col2.split('. ', 1)[1]
+        image = Image.open(BytesIO(b64decode(cols[2].screenshot_as_base64)))
+        text = pytesseract.image_to_string(image, config='--psm 6')
+        results.append([col2, *text.strip().splitlines()])
+    return results
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', '-u', metavar='URL', help='pass single url')
+    parser.add_argument('--input', '-i', metavar='urls.txt', help='get urls from file (one per line)')
+    parser.add_argument('--output', '-o', metavar='out.json', help='save results in json format')
+    parser.add_argument('--print', '-p', action='store_true', help='print to stdout each page result')
+    args = parser.parse_args()
+
+    if not args.output:
+        args.print = True
+
+    if args.input:
+        with open(args.input, 'rt') as f:
+            urls = f.readlines()
+    elif args.url:
+        urls = [args.url]
+    else:
+        print('ERR: specify --url or --input file')
+        return
+
+    driver = init_driver()
+    results = {}
+    for i, url in enumerate(urls):
+        if not url:
+            continue
+        print(f'processing {i+1} / {len(urls)} url')
+        try:
+            results[url] = get_page_data(driver, url)
+        except Exception as e:
+            print(f'ERR: {e!r}')
+            continue
+        if args.print:
+            print(f'URL: {url}')
+            for line in results[url]:
+                print(line)
+
+    if args.output:
+        with open(args.output, 'wt') as f:
+            json.dump(results, f)
+
+
+if __name__ == '__main__':
+    main()
